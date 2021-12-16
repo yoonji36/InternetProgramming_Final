9750060fe6e06289476f6688857ee4d19f97e721
Yoonji Oh <yoonjee36@duksung.ac.kr>
2021-12-16 22:55:48 +0900

    head image 추가

--- mall/models.py
+++ mall/models.py
@@ -6,11 +6,15 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True) #자동 날짜 생성
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str(self):
+    def __str__(self):
         return f'[{self.pk}] {self.title}'
+
+    def get_absolute_url(self):
+        return f'/mall/{self.pk}'
 
     #author
 
